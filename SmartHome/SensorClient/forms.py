599fd9a7a9e16320c26918b51fc5cc22a25fce78
cekefun <cekefun@gmail.com>
2016-04-18 20:25:30 +0200

    heb blijkbaar een beetje veranderd

--- SmartHome/SensorClient/forms.py
+++ SmartHome/SensorClient/forms.py
@@ -1,10 +1,6 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-#    def __init__(self,name,filename):
-#        self.title = name
-#        self.file = filename
-#    title = forms.CharField()
     Uplfile = forms.FileField()
     household = forms.IntegerField()
 
