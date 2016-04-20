580c3f605ba85ef177744fa708e51db3e119f3c6
cekefun <cekefun@gmail.com>
2016-04-20 11:29:13 +0200

    Als er juist geregistered word komet er een alert dat succes zegt. Dit is voorbereiding op exceptions bij inloggen.

--- SmartHome/login/views.py
+++ SmartHome/login/views.py
@@ -6,7 +6,7 @@ from .models import *
 from django.http import HttpResponse
 
 # Create your views here.
-
+#
 
 def loginpage(request):
     template = loader.get_template('login/Login.html')
@@ -14,11 +14,14 @@ def loginpage(request):
     return HttpResponse(template.render(context,request))
 
 def register(request):
+    template = loader.get_template('login/Login.html')
+    context = RequestContext(request)
     if request.method == 'POST':
         form=RegisterForm(request.POST)
         if form.is_valid():
             Register = Registerer()
             Register.Save(form.cleaned_data['Voornaam'],form.cleaned_data['Achternaam'],form.cleaned_data['Usernaam'],form.cleaned_data['Emailadres'],form.cleaned_data['Wachtwoord'])
-            return HttpResponse("Zou gelukt moeten zijn")
+            context = {'message':'SUCCES'}
+            return HttpResponse(template.render(context,request))
     return HttpResponse("Er is iets misgegaan")
 	
