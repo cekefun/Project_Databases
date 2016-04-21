f221c25cb51be866f222638597835d9c6a47556c
FedericoQuin <federico-quin@hotmail.com>
2016-04-21 16:39:48 +0200

    Update urls.py

--- SmartHome/login/urls.py
+++ SmartHome/login/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.register, name='dailyuse'),
-    url(r'^Login/$', views.login, name='login')
-    #url(r'^Registered', views.register, name='Registerer')
+    url(r'^$', views.login, name='login')
+    url(r'^Registered', views.register, name='Registerer')
 ]
