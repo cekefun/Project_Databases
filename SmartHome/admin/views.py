c756f91d209718c83debcbae642fe84c72db3437
cekefun <cekefun@gmail.com>
2016-04-24 14:51:42 +0200

    Als request.session['Language'] nl is worden nu de admin pagina's volledig in het nederlands vertaalt, ook de pop-ups

--- SmartHome/admin/views.py
+++ SmartHome/admin/views.py
@@ -59,7 +59,10 @@ def login(request):
 def users(request):
     if(not request.session['IsAdmin']):
         return HttpResponseNotFound('<h1>Page not found</h1>')
-    template = loader.get_template("admin/UserPage.html")
+    if(request.session['Language'] == 'nl'):
+        template = loader.get_template("admin/UserPage_nl.html")
+    else:
+        template = loader.get_template("admin/UserPage.html")
     searcher=AdminSearch()
     searcher.users()
     results = searcher.getResults()
@@ -72,14 +75,20 @@ def UpdateAdmin(request):
         if(form.is_valid()):
             updater = AdminSearch()
             updater.makeAdmin(request.POST['UserName'])
-            return HttpResponse("everything okay.")
-        return HttpResponse("<h1>Error 404: Something went wrong in making the form</h1>")
-    return HttpResponse("<h1>Error 404: This page should be used with POST</h1>")
+            if(request.session['Language'] == 'nl'):
+                return HttpResponse(request.POST['UserName']+"is nu een admin.")
+            else:
+                return HttpResponse(request.POST['UserName']+"is now an admin.")
+        return HttpResponse("<h1>Error 404: Page not found</h1>")
+    return HttpResponse("<h1>Error 404: Page not found</h1>")
 
 def getreport(request):
     if(not request.session['IsAdmin']):
         return HttpResponseNotFound('<h1>Page not found</h1>')
-    template = loader.get_template("admin/reportForm.html")
+    if(request.session['Language'] == 'nl'):
+        template = loader.get_template("admin/reportForm_nl.html")
+    else:
+        template = loader.get_template("admin/reportForm.html")
     if(request.method == 'POST'):
         form = RequestForm(request.POST)
         if(form.is_valid()):
@@ -87,7 +96,10 @@ def getreport(request):
             startDate=form.cleaned_data['start']
             endDate = form.cleaned_data['to']
             if(endDate < startDate):
-                context = {'message':'Startdate falls after the enddate'}
+                if(request.session['Language'] == 'nl'):
+                    context = {'message':'Startdatum is na de einddatum'}
+                else:
+                    context = {'message':'Startdate falls after the enddate'}
                 return HttpResponse(template.render(context, request))
 
             startDate = datetime.datetime.combine(startDate, datetime.time.min)
@@ -111,7 +123,10 @@ def getreport(request):
             elif (kind == 'year'):
                 table = 'YearData'
             else:
-                context = {'message': 'Please choose a sampletype'}
+                if(request.session['Language'] == 'nl'):
+                    context = {'message': 'Kies alstublieft een type data'}
+                else:
+                    context = {'message': 'Please choose a sampletype'}
                 return HttpResponse(template.render(context, request))
             
             kind = form.cleaned_data['selectregion']
@@ -120,7 +135,10 @@ def getreport(request):
             if (kind == 'streetnamecity'):
                 regionKind = "City"
                 if(form.cleaned_data['city'] == ""):
-                    context = {'message': 'Please write down a city'}
+                    if(request.session['Language'] == 'nl'):
+                        context = {'message': 'Geef alstublieft een stad'}
+                    else:
+                        context = {'message': 'Please write down a city'}
                     return HttpResponse(template.render(context, request))
                 if(form.cleaned_data['streetname'] != ""):
                     aggr.searchStreet(table,startDate,endDate,form.cleaned_data['streetname'],form.cleaned_data['city'])
@@ -128,19 +146,31 @@ def getreport(request):
                     aggr.search(table,startDate,endDate,'City',form.cleaned_data['city'])
             elif (kind == 'postalcode'):
                 if(form.cleaned_data['postalcode'] == ""):
-                    context = {'message': 'Please write down a postalcode'}
+                    if(request.session['Language'] == 'nl'):
+                        context = {'message': 'Geef alstublieft een postcode'}
+                    else:
+                        context = {'message': 'Please write down a postalcode'}
                     return HttpResponse(template.render(context, request))
                 aggr.search(table,startDate,endDate,'PostalCode',form.cleaned_data['postalcode'])
             elif (kind == 'country'):
                 if(form.cleaned_data['country'] == ""):
-                    context = {'message': 'Please write down a country'}
+                    if(request.session['Language'] == 'nl'):
+                        context = {'message': 'Geef alstublieft een land'}
+                    else:
+                        context = {'message': 'Please write down a country'}
                     return HttpResponse(template.render(context, request))
                 aggr.search(table,startDate,endDate,'Country',form.cleaned_data['country'])
             else:
-                context = {'message': 'Please select a region'}
+                if(request.session['Language'] == 'nl'):
+                    context = {'message': 'Selecteer alstublieft een regio'}
+                else:
+                    context = {'message': 'Please select a region'}
                 return HttpResponse(template.render(context, request))
             return HttpResponse(aggr.toJson())
-        context = {'message': 'Please fill in all fields'}
+        if(request.session['Language'] == 'nl'):
+            context = {'message': 'Vul alstublieft alle velden in'}            
+        else:
+            context = {'message': 'Please fill in all fields'}
         return HttpResponse(template.render(context, request))
     context = {'message': ''}
     return HttpResponse(template.render(context, request))
