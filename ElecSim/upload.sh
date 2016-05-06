618fc433a54de5009320b34f7943c2f2be8e8462
cekefun <cekefun@gmail.com>
2016-05-06 15:50:51 +0200

    Update upload.sh
    nu stuurt het scriptje deze minuut en 1 minuut in de TOEKOMST

--- ElecSim/upload.sh
+++ ElecSim/upload.sh
@@ -4,8 +4,8 @@ JSON=MY_CONFIGURE.json
 FAMILY=1
 FILE=MY_FILE.csv
 PROGRAM=main.py
-NOW=$(date +"%Y-%m-%dT%H:%M")
-EARLIER=$(date --date="1 minutes ago" +"%Y-%m-%dT%H:%M")
+NOW=$(date --date="next" +"%Y-%m-%dT%H:%M")
+EARLIER=$(date +"%Y-%m-%dT%H:%M")
 
 python3 $PROGRAM --mode=generate --config_file=$JSON --household=$FAMILY --from=$EARLIER --to=$NOW --output=$FILE
 
